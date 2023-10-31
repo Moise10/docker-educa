11c45047231f49ce5cfb6970badf1ac8d8dcc9ac
moise <mbakopmoise237@gmail.com>
2023-10-31 13:40:54 +0800

    adding the authentication: Login and Logout

--- courses/models.py
+++ courses/models.py
@@ -1,5 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User 
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+from .fields import OrderField
+
 
 
 class Subject(models.Model):
@@ -15,7 +19,8 @@ class Subject(models.Model):
   
 class Course(models.Model):
   owner = models.ForeignKey(User, 
-                            on_delete=models.CASCADE,        related_name='courses_created')
+                            on_delete=models.CASCADE,
+                            related_name='courses_created')
   subject = models.ForeignKey(Subject, 
                             on_delete=models.CASCADE, 
                             related_name='courses')
@@ -39,7 +44,54 @@ class Module(models.Model):
                             related_name="modules")
   title = models.CharField(max_length=200)
   description = models.TextField(blank=True)
+  order = OrderField(blank=True, for_fields=['course'])
+
+  class Meta:
+    ordering = ['order']
 
   def __str__(self):
-    return self.title
+    return f'{self.order}. {self.title}'
+
+
+class Content(models.Model):
+  module = models.ForeignKey(Module, on_delete = models.CASCADE, related_name="contents" )
+  content_type = models.ForeignKey(ContentType, 
+                                  on_delete=models.CASCADE, 
+                                  limit_choices_to={'model__in':( 
+                                    'text', 'video', 'image', 'file'
+                                  )})
+  object_id = models.PositiveIntegerField()
+  item = GenericForeignKey('content_type', 'object_id')
+  order = OrderField(blank=True, for_fields=['module'])
+
+  class Meta:
+    ordering = ['order']
+
+
+class ItemBase(models.Model):
+  owner = models.ForeignKey(User, 
+                          related_name='%(class)s_related', 
+                          on_delete=models.CASCADE)
+  
+  title = models.CharField(max_length=250)
+  created = models.DateTimeField(auto_now_add=True)
+  updated = models.DateTimeField(auto_now=True)
+
+  class Meta:
+    abstract = True
+
+  
+class Text(ItemBase):
+  content = models.TextField()
+
+class File(ItemBase):
+  file = models.FileField(upload_to='files')
+
+
+class Image(ItemBase):
+  file = models.FileField(upload_to='images')
+
+
+class Video(ItemBase):
+  url = models.URLField()
 
