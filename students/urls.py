55e665f410b911645e8e353a86b86a8bbad75ece
moise <mbakopmoise237@gmail.com>
2023-11-04 11:22:33 +0800

    added the cached to the full app

--- students/urls.py
+++ students/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
+
 from . import views
+
 
 
 
@@ -14,8 +17,8 @@ urlpatterns = [
   
   path('courses/',views.StudentCourseListView.as_view(),name='student_course_list'),
 
-  path('course/<pk>/',views.StudentCourseDetailView.as_view(),name='student_course_detail'),
+  path('course/<pk>/',cache_page(60*15)(views.StudentCourseDetailView.as_view()),name='student_course_detail'),
 
-  path('course/<pk>/<module_id>/',views.StudentCourseDetailView.as_view(),     name='student_course_detail_module')
+  path('course/<pk>/<module_id>/', cache_page(60*15)(views.StudentCourseDetailView.as_view()),     name='student_course_detail_module')
                             
 ]
