b04944da1c100701e5fbb653614673b77e27fdef
moise <mbakopmoise237@gmail.com>
2023-11-04 10:13:18 +0800

    adding memcached to cache the subjects in our app

--- courses/views.py
+++ courses/views.py
@@ -9,6 +9,7 @@ from django.apps import apps
 from django.db.models import Count
 from django.views.generic.detail import DetailView
 from students.forms import CourseEnrollForm
+from django.core.cache import cache
 
 
 from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
@@ -173,14 +174,18 @@ class ContentOrderView(CsrfExemptMixin,JsonRequestResponseMixin,View):
       Content.objects.filter(id=id,module__course__owner=request.user).update(order=order)
     return self.render_json_response({'saved': 'OK'})
 
-
 class CourseListView(TemplateResponseMixin, View):
   model = Course
   template_name = 'courses/course/list.html'
 
   def get(self, request, subject=None):
-    subjects = Subject.objects.annotate(total_courses=Count('courses'))
+    subjects = cache.get('all_subjects')
+    if not subjects:
+      subjects = Subject.objects.annotate(total_courses=Count('courses'))
+      cache.set('all_subjects', subjects)
+    
     courses = Course.objects.annotate(total_modules=Count('modules'))
+    
 
     if subject:
       subject = get_object_or_404(Subject, slug=subject)
